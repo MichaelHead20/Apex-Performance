a2a53b3717098ebd7538ffa412db611bc12cc0ee
MichaelHead20 <mick2head@gmail.com>
2026-06-26 23:04:10 +0100

    made the workout-form functional and printed the workouts underneath the form so that people can track what exercises they have done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log(document.getElementById("workout-form"));
+const workoutForm = document.getElementById("workout-form");
+const exerciseInput = document.getElementById("exercise");
+const weightInput = document.getElementById("weight");
+const setsInput = document.getElementById("sets");
+const repsInput = document.getElementById("reps");
+
+workoutForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(event);
+    console.log("Workout submitted!");
+    console.log("Exercise:", exerciseInput.value);
+    console.log("Weight:", weightInput.value);
+    console.log("Sets:", setsInput.value);
+    console.log("Reps:", repsInput.value);
+});
