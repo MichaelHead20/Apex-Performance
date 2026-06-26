9d523d0d50b2b534db0f366eea2111808c08ab55
MichaelHead20 <mick2head@gmail.com>
2026-06-26 23:06:35 +0100

    changes made to the js to make the workout form more functional

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const workoutForm = document.getElementById("workout-form");
+
 const exerciseInput = document.getElementById("exercise");
 const weightInput = document.getElementById("weight");
 const setsInput = document.getElementById("sets");
 const repsInput = document.getElementById("reps");
+
+const workoutCard = document.createElement("div");
+workoutCard.className="workout-card";
+
+const exerciseTitle = document.createElement("h3");
+exerciseTitle.textContent = "Exercise: " + exerciseInput.value;
+
+const exerciseWeight = document.createElement("p");
+exerciseWeight.textContent = "Weight: " + weightInput.value + " kg";
+
+const exerciseSets = document.createElement("p");
+exerciseSets.textContent = "Sets: " + setsInput.value;
+
+const exerciseReps = document.createElement("p");
+exerciseReps.textContent = "Reps: " + repsInput.value;
 
 workoutForm.addEventListener("submit", function (event) {
     event.preventDefault();
