bfd88b1b3e65b51855cb027a615f728565059b60
MichaelHead20 <mick2head@gmail.com>
2026-06-27 12:41:13 +0100

    the workout form now works through js ad presents the exercise done underneath the form

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 const workoutForm = document.getElementById("workout-form");
+const loggedWorkouts = document.getElementById("logged-workouts");
 
 const exerciseInput = document.getElementById("exercise");
-const weightInput = document.getElementById("weight");
+const exerciseWeightInput = document.getElementById("exercise-weight");
 const setsInput = document.getElementById("sets");
 const repsInput = document.getElementById("reps");
-
-const workoutCard = document.createElement("div");
-workoutCard.className="workout-card";
-
-const exerciseTitle = document.createElement("h3");
-exerciseTitle.textContent = "Exercise: " + exerciseInput.value;
-
-const exerciseWeight = document.createElement("p");
-exerciseWeight.textContent = "Weight: " + weightInput.value + " kg";
-
-const exerciseSets = document.createElement("p");
-exerciseSets.textContent = "Sets: " + setsInput.value;
-
-const exerciseReps = document.createElement("p");
-exerciseReps.textContent = "Reps: " + repsInput.value;
 
 workoutForm.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log(event);
-    console.log("Workout submitted!");
-    console.log("Exercise:", exerciseInput.value);
-    console.log("Weight:", weightInput.value);
-    console.log("Sets:", setsInput.value);
-    console.log("Reps:", repsInput.value);
+
+    const workoutCard = document.createElement("div");
+    workoutCard.className = "workout-card";
+
+    const exerciseTitle = document.createElement("h4");
+    exerciseTitle.textContent = exerciseInput.value;
+
+    const exerciseWeight = document.createElement("p");
+    exerciseWeight.textContent = "Weight: " + exerciseWeightInput.value + " kg";
+
+    const exerciseSets = document.createElement("p");
+    exerciseSets.textContent = "Sets: " + setsInput.value;
+
+    const exerciseReps = document.createElement("p");
+    exerciseReps.textContent = "Reps: " + repsInput.value;
+
+    workoutCard.appendChild(exerciseTitle);
+    workoutCard.appendChild(exerciseWeight);
+    workoutCard.appendChild(exerciseSets);
+    workoutCard.appendChild(exerciseReps);
+    loggedWorkouts.appendChild(workoutCard);
+
+    workoutForm.reset();
 });
